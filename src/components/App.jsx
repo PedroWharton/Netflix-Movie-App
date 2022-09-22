d2a30c55eed6be4b04fc0214246c4b9c72bb07f3
PedroWharton <pedrowharton@gmail.com>
2022-09-22 15:35:09 -0300

    login register y watch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import '../stylesheets/App.scss';
 import Home from './Home';
+import Login from './Login';
+import Register from './Register';
+import Watch from './Watch';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Login/>
     </div>
   );
 }
